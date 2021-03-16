56f6afcac444379eea92518649747bff55e3d360
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-03-16 15:07:21 -0300

    Database seeding

--- backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/services/AboutUsService.java
+++ backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/services/AboutUsService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetointegrador.liriodosvalesmodaevangelica.entities.AboutUs;
 import com.projetointegrador.liriodosvalesmodaevangelica.repositories.AboutUsRepository;
@@ -14,6 +15,7 @@ public class AboutUsService {
 	@Autowired
 	private AboutUsRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<AboutUs> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO about_us (description, img_url) VALUES ('Lorem ipsum dolor sit amet...', 'https://www.petz.com.br/blog/wp-content/uploads/2020/07/tipos-de-lirios.jpg');
