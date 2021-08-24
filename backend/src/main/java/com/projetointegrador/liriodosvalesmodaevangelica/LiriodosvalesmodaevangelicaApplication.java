c67157b47f08d2fa6abb6c5bbca741b7e65b83eb
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-08-24 20:31:47 -0300

    First upload test

--- backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/LiriodosvalesmodaevangelicaApplication.java
+++ backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/LiriodosvalesmodaevangelicaApplication.java
@@ -1,13 +1,25 @@
 package com.projetointegrador.liriodosvalesmodaevangelica;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LiriodosvalesmodaevangelicaApplication {
+import com.projetointegrador.liriodosvalesmodaevangelica.services.S3Service;
 
+@SpringBootApplication
+public class LiriodosvalesmodaevangelicaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LiriodosvalesmodaevangelicaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\testeUploadAWS.jpg");
 	}
 
 }
