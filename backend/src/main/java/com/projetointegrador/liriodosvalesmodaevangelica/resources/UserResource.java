aa3a32165da21179a698dc2882d142a538d83795
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-10-19 22:43:15 -0300

    Route 'U' disabled

--- backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/resources/UserResource.java
+++ backend/src/main/java/com/projetointegrador/liriodosvalesmodaevangelica/resources/UserResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetointegrador.liriodosvalesmodaevangelica.dtos.UserDTO;
@@ -19,7 +18,7 @@ import com.projetointegrador.liriodosvalesmodaevangelica.services.UserService;
 
 @RestController
 // Comentei o campo abaixo, pois não faz sentido ter esse endpoint liberado.
-@RequestMapping(value = "/users")
+//@RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
