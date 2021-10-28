29633d1e34d892b4e9107d2a12f4e7df0608e40b
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-10-28 20:05:47 -0300

    GA test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,12 @@ import './app.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
 import { ToastContainer } from 'react-toastify';
+import ReactGA from 'react-ga';
 
 function App() {
+  const TRACKING_ID = "G-P7N1CMRFMG"; // YOUR_OWN_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <>
       <Routes />
