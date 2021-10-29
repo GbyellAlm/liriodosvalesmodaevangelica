87653a6b8ffdaa37bfb2847587da57c9e818fdff
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-10-29 20:17:06 -0300

    Set GA and adjustments to Whatsapp btn times

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,8 @@ import './app.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
 import { ToastContainer } from 'react-toastify';
-//import GA4React from "ga-4-react";
-
-//const ga4react = new GA4React("G-9T6HQD8EJV");
 
 function App() {
-  //ga4react.initialize();
-
   return (
     <>
       <Routes />
